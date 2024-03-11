2151a59f7ff32588b8e93ffb4298c8a47a8418d7
rashidjatoi <rashidjatoi31@gmail.com>
2024-03-12 00:45:13 +0500

    Using functions inside the jsx

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,8 +1,11 @@
 function Hello() {
 
     let name = 'rashidd';
+    let fullName = () => {
+        return "rashid ali jatoi";
+    }
     return <div>
-        <h2>My name is {name}</h2>
+        <h2>My name is {name} & {fullName} </h2>
     </div>
 
 }
